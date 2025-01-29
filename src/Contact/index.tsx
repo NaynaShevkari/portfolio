13be62ca8a868e6a03af239e57386b64d635dcfe
Nayna <shevkarinayna@gmail.com>
2025-01-28 20:52:49 -0500

    Links opening in new browser tabs

--- src/Contact/index.tsx
+++ src/Contact/index.tsx
@@ -5,8 +5,8 @@ export default function Contact(){
             <h1>CONTACT</h1>
             <h4>Call me at: +1 (609) 982-1652</h4>
             <h4>Reach out to me at: shevkari.n@northeastern.edu</h4>
-            <h4> <a href="https://www.linkedin.com/in/nayna-shevkari/" >LinkedIn</a></h4>
-            <h4> <a href="https://github.com/NaynaShevkari" >GitHub</a></h4>            
+            <h4> <a href="https://www.linkedin.com/in/nayna-shevkari/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h4>
+            <h4> <a href="https://github.com/NaynaShevkari" target="_blank" rel="noopener noreferrer">GitHub</a></h4>            
         </div>
     );
 }
